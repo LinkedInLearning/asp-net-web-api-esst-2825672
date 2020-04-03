6fddd5352b0d6d5dc60be736e546341d4e10fc0c
Christian Wenz <info@christianwenz.de>
2020-04-03 21:08:11 +0200

    06_09 END STATE

---
+++ HPlusSport/HPlusSport.ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HPlusSport.ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var discoveryDocument = await client.GetDiscoveryDocumentAsync(
+                "http://localhost:51959");
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(
+                new ClientCredentialsTokenRequest
+                {
+                    Address = discoveryDocument.TokenEndpoint,
+                    ClientId = "client",
+                    ClientSecret = "H+ Sport",
+                    Scope = "hps-api"
+                });
+
+            Console.WriteLine($"Token: {tokenResponse.AccessToken}");
+        }
+    }
+}
