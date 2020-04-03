9fb40bddc30dba403942d69c7fe331aa2aecdf70
Christian Wenz <info@christianwenz.de>
2020-04-03 21:05:10 +0200

    06_03 END STATE

---
+++ HPlusSport/HPlusSport.Web/wwwroot/js/api.js
@@ -0,0 +1,23 @@
+﻿let url = "https://localhost:44329/products";
+
+let productsList = document.getElementById("products-list");
+
+if (productsList) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => showProducts(data))
+        .catch(ex => {
+            alert("Fehler ...");
+            console.log(ex);
+        });
+}
+
+function showProducts(products) {
+    products.forEach(product => {
+        let li = document.createElement("li");
+        let text = `${product.name} (${product.price}€)`;
+        li.appendChild(
+            document.createTextNode(text));
+        productsList.appendChild(li);
+    });
+}
