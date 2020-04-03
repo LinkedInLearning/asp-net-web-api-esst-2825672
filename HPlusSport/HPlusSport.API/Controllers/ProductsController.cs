37ffc7939acfa3fa36c0dc9c4b282c66be783ca0
Christian Wenz <info@christianwenz.de>
2020-04-03 21:01:20 +0200

    04_07 END STATE

--- HPlusSport/HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport/HPlusSport.API/Controllers/ProductsController.cs
@@ -109,5 +109,20 @@ namespace HPlusSport.API.Controllers
 
             return NoContent();
         }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> DeleteProduct([FromRoute]int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
     }
 }
