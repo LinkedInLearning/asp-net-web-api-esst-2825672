c5f710bd1340c0f7075ec76bd29be5e9fe35d6c9
Christian Wenz <info@christianwenz.de>
2020-04-03 20:53:11 +0200

    02_06 END STATE

--- HPlusSport/HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport/HPlusSport.API/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using HPlusSport.API.Models;
 
 namespace HPlusSport.API.Controllers
 {
@@ -11,10 +12,18 @@ namespace HPlusSport.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private readonly ShopContext _context;
+
+        public ProductsController(ShopContext context) {
+            _context = context;
+
+            _context.Database.EnsureCreated();
+        }
+
         [HttpGet]
-        public string GetProducts()
+        public IEnumerable<Product> GetAllProducts()
         {
-            return "OK.";
+            return _context.Products.ToArray();
         }
     }
 }
