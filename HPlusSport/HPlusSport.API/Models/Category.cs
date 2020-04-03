db5e62a92879c520c6043abff3bee1f2e3e1da65
Christian Wenz <info@christianwenz.de>
2020-04-03 20:49:50 +0200

    02_03 END STATE

---
+++ HPlusSport/HPlusSport.API/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HPlusSport.API.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual List<Product> Products { get; set; }
+    }
+}
---
+++ HPlusSport/HPlusSport.API/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HPlusSport.API.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Sku { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public int CategoryId { get; set; }
+        [JsonIgnore]
+        public virtual Category Category { get; set; }
+    }
+}
