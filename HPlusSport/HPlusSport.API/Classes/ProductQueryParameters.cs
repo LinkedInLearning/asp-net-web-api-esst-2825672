b700bf654b8960560ff5da5aca5dda42a7af0143
Christian Wenz <info@christianwenz.de>
2020-04-03 20:57:40 +0200

    03_04 END STATE

--- HPlusSport/HPlusSport.API/Classes/ProductQueryParameters.cs
+++ HPlusSport/HPlusSport.API/Classes/ProductQueryParameters.cs
@@ -8,6 +8,9 @@ namespace HPlusSport.API.Classes
     public class ProductQueryParameters : QueryParameters
     {
         public decimal? MinPrice { get; set; }
-        public decimal? MaxPrice { get; set; }        
+        public decimal? MaxPrice { get; set; }
+
+        public string Sku { get; set; }
+        public string Name { get; set; }
     }
 }
--- HPlusSport/HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport/HPlusSport.API/Controllers/ProductsController.cs
@@ -35,6 +35,18 @@ namespace HPlusSport.API.Controllers
                     );
                 }
 
+            if (!string.IsNullOrEmpty(queryParameters.Sku))
+            {
+                products = products.Where(p => p.Sku == queryParameters.Sku);
+            }
+
+            if (!string.IsNullOrEmpty(queryParameters.Name))
+            {
+                products = products.Where(
+                    p => p.Name.ToLower().Contains(
+                        queryParameters.Name.ToLower()));
+            }
+
             products = products
                 .Skip(queryParameters.Size * (queryParameters.Page - 1))
                 .Take(queryParameters.Size);
