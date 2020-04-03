3984a167815175a393c091bbc748019a4f4dfd8a
Christian Wenz <info@christianwenz.de>
2020-04-03 20:44:01 +0200

    02_01 END STATE

---
+++ HPlusSport/HPlusSport.API/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HPlusSport.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public void GetProducts()
+        {
+
+        }
+    }
+}
